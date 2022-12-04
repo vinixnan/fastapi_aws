4389fab1a90f3af80c7b82f9997aa455ffcaf274
Vinicius Renan de Carvalho <viniciusrenan.carvalho@gmail.com>
2022-12-04 00:06:48 -0300

    Current

--- generate_docker.sh
+++ generate_docker.sh
@@ -1,12 +1,3 @@
-#docker build -t fastapi_aws:v1 .
-export DOCKER_CLI_EXPERIMENTAL=enabled
-docker buildx create --use --name multi-arch-builder
-docker buildx build --platform=linux/amd64 -t fastapi_aws:v1 .
-
 myregion=$(aws configure get region)
-
-aws ecr create-repository --repository-name fastapi_aws --region $myregion > creationJson.json
-repositoryUri=$(cat creationJson.json | jq ".repository" | jq ".repositoryUri" | xargs)
-aws ecr get-login-password --region $myregion | docker login --username AWS --password-stdin $repositoryUri
-docker tag fastapi_aws:v1 $repositoryUri:v1
-docker push $repositoryUri:v1
+aws ecr create-repository --repository-name fastapi_aws --region $myregion
+source update_docker.sh
--- update_docker.sh
+++ update_docker.sh
@@ -1,7 +1,10 @@
-docker buildx build --platform=linux/amd64 -t fastapi_aws:v1 .
+export DOCKER_CLI_EXPERIMENTAL=enabled
+docker buildx create --use
 myregion=$(aws configure get region)
 repositoryUri=$(aws ecr describe-repositories --repository-name fastapi_aws | jq -r ".repositories[0].repositoryUri")
 echo $repositoryUri at $myregion
 aws ecr get-login-password --region $myregion | docker login --username AWS --password-stdin $repositoryUri
-docker tag fastapi_aws:v1 $repositoryUri:v1
-docker push $repositoryUri:v1
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  --push -t $repositoryUri:v2 \
+  -f Dockerfile .
